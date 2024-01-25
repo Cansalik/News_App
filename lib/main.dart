bc533c8c742e78c8750ae6362ff7b1738b63b465
“cansalik” <neset.salik@gmail.com>
2024-01-25 18:03:32 +0300

    Code Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:news_app/views/homescreen.dart';
 import 'package:news_app/views/landingpage.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
       ),
-      home: LandingPage(),
+      home: HomePage(),
     );
   }
 }
